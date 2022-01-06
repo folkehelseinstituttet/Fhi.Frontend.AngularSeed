2199ef52f6e7085b7eb3b987f0e951d12e99f722
Bernt Andersen <bernt.andersen@fhi.no>
2022-01-06 16:54:05 +0100

    Update src for seed 2

--- .docker/angular-seed-2/src/app/app-routing.module.ts
+++ .docker/angular-seed-2/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  pathMatch: 'full',
+  redirectTo: 'demo-feature'
+}, {
+  path: 'demo-feature',
+  loadChildren: () => import('./demo-feature/demo-feature.module').then(m => m.DemoFeatureModule)
+}, {
+  path: '**',
+  redirectTo: 'demo-feature'
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ .docker/angular-seed-2/src/app/demo-feature/demo-feature.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-demo-feature',
+  templateUrl: './demo-feature.component.html'
+})
+export class DemoFeatureComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
